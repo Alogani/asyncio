2db5e30d4c999114e4adb7567eb74d17d89602d0
Alogani <77880767+Alogani@users.noreply.github.com>
2024-04-25 13:36:45 +0200

    asyncbuffer implementation: tests needed

--- src/asyncio/asyncbuffer.nim
+++ src/asyncio/asyncbuffer.nim
@@ -1,7 +1,7 @@
 import ./exports/asynciobase {.all.}
 import ./private/buffer
 
-const defaultBufSize = 1024
+const defaultBufSize = 1024 # Is this a good default ?
 
 type
     AsyncBuffer* = ref object of AsyncIoBase
@@ -14,8 +14,8 @@ type
         stream: AsyncIoBase
 
 proc new*(T: type AsyncBuffer, stream: AsyncIoBase, readBufSize = defaultBufSize, writeBufSize = defaultBufSize): T
-proc flush*(self: AsyncBuffer): Future[int]
-proc fillBuffer*(self: AsyncBuffer, count: int): Future[void]
+proc flush*(self: AsyncBuffer, cancelFut: Future[void] = nil): Future[int]
+proc fillBuffer*(self: AsyncBuffer, count: int, cancelFut: Future[void] = nil): Future[void]
 proc bufLen*(self: AsyncBuffer): tuple[readBuffer, writeBuffer: int]
 method readAvailableUnlocked(self: AsyncBuffer, count: int, cancelFut: Future[void]): Future[string]
 method readChunkUnlocked(self: AsyncBuffer, cancelFut: Future[void]): Future[string]
@@ -32,17 +32,38 @@ proc new*(T: type AsyncBuffer, stream: AsyncBase, readBufSize = defaultBufSize, 
         stream: stream)
     result.init(readLock: stream.readLock, writeLock: stream.writeLock)
 
-proc flush*(self: AsyncBuffer): Future[int] =
+proc flush*(self: AsyncBuffer, cancelFut: Future[void] = nil): Future[int] =
     var data = readBuffer.readAll()
     if data.len() != 0:
-        return self.stream.write(data)
+        return self.stream.write(data, cancelFut)
 
-proc fillBuffer*(self: AsyncBuffer, count: int = 0): Future[int] {.async.} =
+proc fillBuffer*(self: AsyncBuffer, count: int = 0, cancelFut: Future[void] = nil): Future[int] {.async.} =
     ## If count is not given, use the one of the object
-    let data = await self.stream.read(if count == 0: self.writeBufSize else: count)
+    let data = await self.stream.read(if count == 0: self.readBufSize else: count, cancelFut)
     result = data.len()
     self.writeBuffer(data)
 
 proc bufLen*(self: AsyncBuffer): tuple[readBuffer, writeBuffer: int] =
     (self.readBuffer.len(), self.writeBuffer.len())
  
+method readAvailableUnlocked(self: AsyncBuffer, count: int, cancelFut: Future[void]): Future[string] {.async.} =
+    if self.readBuffer.len() < count:
+        discard await self.fillBuffer(max(count, self.readBufSize), cancelFut)
+    return self.readBuffer.read(count)
+
+method readChunkUnlocked(self: AsyncBuffer, cancelFut: Future[void]): Future[string] {.async.} =
+    if self.readBuffer.isEmpty():
+        return await self.stream.readChunk()
+    else:
+        return await self.stream.read(readBufSize, cancelFut)
+
+method writeUnlocked(self: AsyncBuffer, data: string, cancelFut: Future[void]): Future[int] {.async.} =
+    let dataLen = data.len()
+    self.writeBuffer.write(data)
+    if self.writeBuffer.len() > self.writeBufSize:
+        return await self.stream.write(self.writeBuffer.readAll(), cancelFut)
+    else:
+        return dataLen
+
+method close*(self: AsyncBuffer) =
+    self.stream.close()
